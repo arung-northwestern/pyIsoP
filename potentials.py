cda24ad20a8ac648236e80dc9eaa3382038cdf99
Arun Gopalan <arungopalan2020@u.northwestern.edu>
2019-02-25 21:05:16 -0600

    Added LJ and LJFH

--- potentials.py
+++ potentials.py
@@ -1,5 +1,5 @@
 class potentials:
-    
+
         """
          ######
          #     #  ####  ##### ###### #    # ##### #   ##   #       ####
@@ -9,13 +9,17 @@ class potentials:
          #       #    #   #   #      #   ##   #   # #    # #      #    #
          #        ####    #   ###### #    #   #   # #    # ######  ####
          # TODO: Add the Feynman-Hibbs potential along with the functional form to the list
+         # TODO: Add vectorize decorator for the potentials
+         # TODO: Add the ValueErrors and TypeErrors to potentials for safety
          
         """
+        from numba import vectorize
         def __init__(self):
             pass
+        # * The Lennard-Jones Potential
 
-        def lj(self, rsq, epsilon =36.7 , sigma = 2.958): # * Defaults are for hydrogen
-    
+        @vectorize(['float64(float64, float64, float64)'], target='cpu')
+        def lj(self, rsq, epsilon, sigma): 
             """ Description
 
             :type rsq: float
@@ -31,11 +35,50 @@ class potentials:
             ValueError: rsq cannot be zero
 
             :rtype: The Lennard-Jones potential value in 'float'.
-            """        
+            """
             self.epsilon = epsilon
             self.sigma = sigma
             return (4*self.epsilon) * (((self.sigma)**12/(rsq)**6) - ((self.sigma)**6/(rsq)**3))
-        # def ljfh(self, rsq, epsilon=36.7, sigma=2.958):
-        #     self.epsilon = epsilon
-        #     self.sigma = sigma
-        #     return (4*self.epsilon) * (((self.sigma)**12/(rsq)**6) - ((self.sigma)**6/(rsq)**3))
+
+        # * Lennard Jones with the Feynman-Hibbs correction    
+        @vectorize(['float64(float64, float64, float64)'], target='cpu')
+        def ljfh(self, rsq, epsilon, sigma, rmass, T):
+            """ Description
+
+            :type rsq: float
+            :param rsq: squared distance between atoms
+
+            :type epsilon: float 
+            :param epsilon: Depth of the potential well in [K]
+
+            :type sigma: float
+            :param sigma: Zero-energy distance in angstrom
+            
+            :type rmass: float
+            :param rmass: Reduced mass of the pair system m1*m2/(m1+m2)
+            
+            :type T: float
+            :param T: Temperature in [K]
+            
+            :raises: 
+            ValueError: rsq cannot be zero
+            TypeError: Provide T and rmass for Feynman Hibbs correction
+
+            :rtype: The Lennard-Jones potential value in 'float'.
+            """
+
+            
+            self.epsilon = epsilon
+            self.sigma = sigma
+            self.rmass = rmass
+            self.T= T
+
+            # * Let's convert everything into SI unites to evaluate the Feynman Hibbs constant clearly: hcut**2/[(r**2)(24*pi*m*k_B*T)]
+            hcut = 1.0545718E-34  # * Planck's constant / 2*pi in Js or kg m2 s-1
+            angs = 1E-10          # * angstrom in m
+            amu = 1.66054E-27    # * amu in kg
+            k_B = 1.38065852E-27  # * Boltzmann constant in J/K or kg m2 s-2 K-1
+
+            const = hcut**2 / ((rsq*angs**2)*(24*rmass*amu*k_B*self.T)) # * Note  that this constant is dimensionless
+
+            return (4*self.epsilon) * (((self.sigma)**12/(rsq)**6) - ((self.sigma)**6/(rsq)**3)) + const*(4*self.epsilon) * (132*((self.sigma)**12/(rsq)**6) - 30*((self.sigma)**6/(rsq)**3))
